d45ab75588722cc6c2f881ec3d45f7b57ef9b8f6
Julija Tarasova <julija.jeremejeva@gmail.com>
2020-10-29 21:20:03 +0200

    Parse some strings

---
+++ src/main/scala/StringFun.scala
@@ -0,0 +1,12 @@
+object StringFun extends App {
+  def sumNumbers(text: String): Int = {
+
+//    val numPattern = "[0-9]+".r
+//    numPattern.findAllIn(text).toSeq.map(_.toInt).foldLeft(0)(_ + _)
+
+    ("[0-9]+".r findAllIn text).map(_.toInt).sum
+  }
+
+  println(sumNumbers("Valdis has 3 cows and 58 sheep and 11horses")) //should print 72
+  //so pure split will not work real data is messy!}
+}
